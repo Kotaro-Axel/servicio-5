b19e99a8e464459a9c77e92ccc7897b9f0348f4f
Kotaro-Axel <191246@ids.upchiapas.edu.mx>
2022-06-03 08:43:36 -0500

    2nd commit

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const errorhandler = require('errorhandler')
 
 let store = {
   posts: [
-    {name: 'Top 10 ES6 Features every Web Developer must know',
+    {name: 'TESTTING POST',
     url: 'https://webapplog.com/es6',
-    text: 'This essay will give you a quick introduction to ES6. If you don’t know what is ES6, it’s a new JavaScript implementation.',
+    text: 'Mi primera publicacion',
     comments: [
-      {text: 'Cruel…..var { house, mouse} = No type optimization at all'},
-      {text: 'I think you’re undervaluing the benefit of ‘let’ and ‘const’.'},
-      {text: '(p1,p2)=>{ … } ,i understand this ,thank you !'}
+      {text: 'Hi from Tuxtla!'},
+      {text: 'Cheers!'},
+      {text: 'Good job'}
     ]
     }
   ]
